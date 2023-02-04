1b82aef701198ec8aff937f4035e8178d30160dc
遗憾哇 <83404297+ilhmtfmlt2@users.noreply.github.com>
2023-02-04 23:06:24 +0800

    主要的文件

---
+++ run.py
@@ -0,0 +1,4 @@
+import os
+os.system("python ./main.py")
+os.system("python ./config.py")
+os.system("python ./time.py")
---
+++ time.py
@@ -0,0 +1,94 @@
+from requests import get
+from time import sleep
+
+# url = "https://www.epicgames.com/store/zh-CN/free-games"
+url = "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=CN&allowCountries=CN"
+response = get(url)
+hashMap = response.json()
+ls = hashMap["data"]["Catalog"]["searchStore"]["elements"]
+now = []
+soon = []
+
+
+def getTitle(item):
+    s = item.get("title", "")  # s might be have '《' or '》'
+    lst = s.strip("《").split("》")
+    return "".join(lst)
+
+
+def getOriginalPrice(item):
+    details = item.get("price").get("totalPrice")
+    return int(details.get("originalPrice", 0))
+
+
+def isFree(item):  # Well this function is poorly designed
+    if getOriginalPrice(item) == 0:
+        return False
+
+    if item.get("promotions", None) is not None:
+        if len(item["promotions"]["promotionalOffers"]) != 0:
+            if item["promotions"]["promotionalOffers"][0]["promotionalOffers"][0]["discountSetting"][
+                "discountPercentage"] == 0:
+                now.append(
+                    f"{getTitle(item):25s}{getTime(item)}")
+                return True
+
+        elif len(item["promotions"]['upcomingPromotionalOffers']) != 0:
+            if item["promotions"]['upcomingPromotionalOffers'][0]["promotionalOffers"][0]["discountSetting"][
+                "discountPercentage"] == 0:
+                soon.append(
+                    f"{getTitle(item):25s}{getTime(item)}")
+                return True
+    else:
+        return False  # It might have a bug,maybe I should check it more.
+
+
+def getTime(item):
+    if item.get("promotions", None) is not None:
+        if len(item["promotions"]["promotionalOffers"]) != 0:
+            startDate = item["promotions"]["promotionalOffers"][0]['promotionalOffers'][0]['startDate'].split("T")[0]
+            endDate = item["promotions"]["promotionalOffers"][0]['promotionalOffers'][0]['endDate'].split("T")[0]
+            return f"{startDate}至{endDate}"
+        elif len(item["promotions"]['upcomingPromotionalOffers']) != 0:
+            startDate = \
+                item["promotions"]["upcomingPromotionalOffers"][0]['promotionalOffers'][0]['startDate'].split("T")[0]
+            endDate = item["promotions"]["upcomingPromotionalOffers"][0]['promotionalOffers'][0]['endDate'].split("T")[
+                0]
+            return f"{startDate}至{endDate}"
+
+
+def main():
+    for item in ls:
+        item.pop('id', "")
+        item.pop('namespace', "")
+        item.pop('description', "")
+        item.pop('url', "")
+        item.pop('keyImages', "")
+        item.pop('categories', "")
+        item.pop('tags', "")
+        item.pop('catalogNs', "")
+        item.pop('offerMappings', "")
+        item.pop('items', "")
+        item.pop('expiryDate', "")
+        item.pop('customAttributes', "")
+        item.pop('offerType', "")
+        item.pop('productSlug', "")
+        item.pop('isCodeRedemptionOnly', "")  # I don't know what is isCodeRedemptionOnly mean.
+        item.pop('seller', "")
+        item.pop('status', "")
+        item.pop('effectiveDate', "")
+        item.pop('urlSlug', "")
+        item.pop('', "")
+        #   It's not necessary but easy for me to maintain.
+
+        isFree(item)
+
+    print("- ## 截止日期".center(0))
+    print()
+    print()
+    for item in now:
+        print(item)
+    for item in soon:
+        print(item)
+if __name__ == '__main__':
+    main()
