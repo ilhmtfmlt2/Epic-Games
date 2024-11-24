cb971c97750ffc30bda869a355addb72f5377c86
遗憾哇 <83404297+ilhmtfmlt2@users.noreply.github.com>
2024-11-24 16:02:34 +0800

    Update Epic.py
    修复了获取到不免费的游戏加入了价格判定

--- Epic.py
+++ Epic.py
@@ -16,13 +16,14 @@ def format_date(date_str: str) -> str:
 
 
 def get_free_games() -> dict:
-    """获取 Epic 限免游戏信息"""
+    """获取 Epic 限免游戏信息，过滤非免费游戏"""
     games = {'free_now': [], 'free_next': []}
     base_store_url = 'https://store.epicgames.com'
-    api_url = 'https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?country=CN'
+    api_url = 'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=zh-CN&country=CN&allowCountries=CN'
     resp = requests.get(api_url)
     for element in resp.json()['data']['Catalog']['searchStore']['elements']:
         if promotions := element.get('promotions'):
+            # 基本游戏信息
             game = {
                 'title': element['title'],
                 'publisher': element.get('seller', {}).get('name', '未知发行商'),
@@ -30,18 +31,26 @@ def get_free_games() -> dict:
                 'origin_price': element['price']['totalPrice']['fmtPrice']['originalPrice'],
                 'store_url': f"{base_store_url}/p/{element['catalogNs']['mappings'][0]['pageSlug']}" if element['catalogNs']['mappings'] else base_store_url,
             }
+
+            # 当前促销（本周限免）
             if offers := promotions.get('promotionalOffers'):
-                game.update({
-                    'start_date': format_date(offers[0]['promotionalOffers'][0]['startDate']),
-                    'end_date': format_date(offers[0]['promotionalOffers'][0]['endDate']),
-                })
-                games['free_now'].append(game)
+                discount_price = element['price']['totalPrice']['fmtPrice']['discountPrice']
+                # 仅当价格为零时，添加到本周限免
+                if discount_price == '0':
+                    game.update({
+                        'start_date': format_date(offers[0]['promotionalOffers'][0]['startDate']),
+                        'end_date': format_date(offers[0]['promotionalOffers'][0]['endDate']),
+                    })
+                    games['free_now'].append(game)
+
+            # 即将促销（下周限免）
             if offers := promotions.get('upcomingPromotionalOffers'):
                 game.update({
                     'start_date': format_date(offers[0]['promotionalOffers'][0]['startDate']),
                     'end_date': format_date(offers[0]['promotionalOffers'][0]['endDate']),
                 })
                 games['free_next'].append(game)
+
     return games
 
 
